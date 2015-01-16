0a523a051163a344f6963f64654ac357d024b539
Jari Hanhela <jarihanhela@gmail.com>
2015-01-16 17:33:56 +0200

    The internal structure of the map module was not consistent with other modules.

--- scripts/js/world_controller.js
+++ scripts/js/world_controller.js
@@ -8,6 +8,7 @@ define(["map", "player"], function(Map, Player) {
         var camera;
         var player;
         var walls = [];
+        var map;
 
         function constructor() {
             initialize();
@@ -37,23 +38,22 @@ define(["map", "player"], function(Map, Player) {
         }
 
         function initializeMap() {
-            // TODO Map constructed wrong
-            var map = new Map();
-            var mapLines = Map.map.split("\n");
+            map = new Map();
+            var mapLines = map.getMap().split("\n");
             for (var lineIndex = 0; lineIndex < mapLines.length; lineIndex++) {
                 var line = mapLines[lineIndex];
                 for (var charIndex = 0; charIndex < line.length; charIndex++) {
-                    insertGameplayObjectToWorld(line.charAt(charIndex), charIndex * Map.getTileSize(), 0, lineIndex * Map.getTileSize());
+                    insertGameplayObjectToWorld(line.charAt(charIndex), charIndex * map.getTileSize(), 0, lineIndex * map.getTileSize());
                 }
             }
 
             // Floor
-            var geometry = new THREE.PlaneGeometry(Map.getWidth(), Map.getHeight(), 1, 1);
+            var geometry = new THREE.PlaneGeometry(map.getWidth(), map.getHeight(), 1, 1);
             var material = new THREE.MeshBasicMaterial({map: gameplayScene.getApplication().getTextureContainer().getTextureByName("floor")});
             var floor = new THREE.Mesh(geometry, material);
-            floor.position.x = Map.getWidth() / 2 - (Map.getTileSize() / 2);
-            floor.position.y = -Map.getTileSize() / 2;
-            floor.position.z = Map.getHeight() / 2 - (Map.getTileSize() / 2);
+            floor.position.x = map.getWidth() / 2 - (map.getTileSize() / 2);
+            floor.position.y = -map.getTileSize() / 2;
+            floor.position.z = map.getHeight() / 2 - (map.getTileSize() / 2);
             floor.rotation.x = -90 * Math.PI / 180;
             floor.castShadow = true;
             floor.receiveShadow = true;
@@ -63,27 +63,27 @@ define(["map", "player"], function(Map, Player) {
             // TODO Shadows look strange
             var light = new THREE.DirectionalLight(0xf6e86d, 1);
             // light.shadowCameraVisible = true;
-            light.position.x = -Map.getTileSize();
-            light.position.y = Map.getTileSize() + 5;
-            light.position.z = -Map.getTileSize();
-            light.target.position.x = Map.getTileSize() * 5;
+            light.position.x = -map.getTileSize();
+            light.position.y = map.getTileSize() + 5;
+            light.position.z = -map.getTileSize();
+            light.target.position.x = map.getTileSize() * 5;
             light.target.position.y = 80;
-            light.target.position.z = Map.getTileSize() * 5;
+            light.target.position.z = map.getTileSize() * 5;
             light.castShadow = true;
             scene.add(light);
         }
 
         function initializeWorld() {
-            initializeSky();
             initializeMap();
+            initializeSky();
         }
 
         function initializeSky() {
             var sky = new THREE.Mesh(
                 new THREE.CubeGeometry(5000, 5000, 5000),
                 new THREE.MeshFaceMaterial(gameplayScene.getApplication().getTextureContainer().getTextureByName("skybox")));
-            sky.position.x = Map.getWidth() / 2;
-            sky.position.z = Map.getHeight() / 2;
+            sky.position.x = map.getWidth() / 2;
+            sky.position.z = map.getHeight() / 2;
             scene.add(sky);
         }
 
@@ -94,9 +94,9 @@ define(["map", "player"], function(Map, Player) {
                 var wall = new THREE.Mesh(geometry.clone(), material);
                 wall.position.x = x;
                 wall.position.z = z;
-                wall.scale.x = Map.getTileSize();
-                wall.scale.y = Map.getTileSize();
-                wall.scale.z = Map.getTileSize();
+                wall.scale.x = map.getTileSize();
+                wall.scale.y = map.getTileSize();
+                wall.scale.z = map.getTileSize();
                 wall.castShadow = true;
                 wall.receiveShadow = true;
                 scene.add(wall);
